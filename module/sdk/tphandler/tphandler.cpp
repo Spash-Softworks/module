5926565393c3d8b9ace6ae96e3bea2a267d967eb
Kamerzystanasyt <abuse@queenmc.pl>
2026-06-10 18:01:22 +0200

    it worky

--- module/sdk/tphandler/tphandler.cpp
+++ module/sdk/tphandler/tphandler.cpp
@@ -21,6 +21,11 @@ void tphandler_t::init(uintptr_t base)
 	uintptr_t scriptcontext = 	taskscheduler->get_scriptcontext(datamodel);
 	int gameloaded = 			taskscheduler->get_gameloaded(datamodel);
 
+	// jobs
+	uintptr_t hybridscripts = 	taskscheduler->get_job_by_name(base, "WaitingHybridScriptsJob");
+	uintptr_t heartbeat = 		taskscheduler->get_job_by_name(base, "Heartbeat");
+	uintptr_t renderjob = 		taskscheduler->get_job_by_name(base, "RenderJob");
+
 	using printfunc = void(__cdecl*)(int, const char*, ...);
     auto print = reinterpret_cast<printfunc>(base + Offsets::Internal_Offsets::Print);
 
@@ -32,7 +37,11 @@ void tphandler_t::init(uintptr_t base)
     print(0, "scriptcontext @ 0x%llX", 	scriptcontext);
 	print(0, "gameloaded @ %d", 		gameloaded);
     print(0, "");
-    
+	print(0, "WaitingHybridScriptsJob @ 0x%llX", 	hybridscripts);
+    print(0, "Heartbeat @ 0x%llX", 					heartbeat);
+    print(0, "RenderJob @ 0x%llX", 					renderjob);
+	print(0, "");
+
     auto now = std::chrono::steady_clock::now();
     auto end = std::chrono::duration_cast<std::chrono::seconds>(now - begin).count();
     print(0, "%s loaded in %llds", "femboyware", end);
