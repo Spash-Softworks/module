f68e7eb69da7461e864d34a713e208b0d3d135e1
Kamerzystanasyt <abuse@queenmc.pl>
2026-06-10 17:09:54 +0200

    im soo gay

--- module/sdk/scheduler/scheduler.h
+++ module/sdk/scheduler/scheduler.h
@@ -4,8 +4,8 @@
 struct taskscheduler_t {
     uintptr_t get_datamodel(uintptr_t base);
 	uintptr_t get_placeid(uintptr_t datamodel);
-	uintptr_t get_gameloaded(uintptr_t datamodel);
 	uintptr_t get_scriptcontext(uintptr_t datamodel);
+	int get_gameloaded(uintptr_t datamodel);
 
 
     uintptr_t get_job_by_name(uintptr_t base, const std::string& name);
